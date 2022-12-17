9fffd8cbd39cec60baf498b603e7534d197c500c
amings <amings0622@gmail.com>
2022-12-17 20:29:45 +0800

    fix : SvcResModel no convertor問題

--- mingle-core/src/main/java/io/github/amings/mingle/svc/SvcResModel.java
+++ mingle-core/src/main/java/io/github/amings/mingle/svc/SvcResModel.java
@@ -1,11 +1,14 @@
 package io.github.amings.mingle.svc;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 /**
  * Base Svc response model
  *
  * @author Ming
  */
 
+@JsonSerialize
 public class SvcResModel {
 
 }
