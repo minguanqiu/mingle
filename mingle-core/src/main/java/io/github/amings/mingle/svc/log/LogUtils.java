6bca60b4a638b6c1c5e7966aca67a00b349ba18a
amings <amings0622@gmail.com>
2023-04-12 20:15:08 +0800

    Bugfix : Aop logging without Svc throw NullPointerException

--- mingle-core/src/main/java/io/github/amings/mingle/svc/log/LogUtils.java
+++ mingle-core/src/main/java/io/github/amings/mingle/svc/log/LogUtils.java
@@ -11,11 +11,13 @@ public class LogUtils {
     public static SvcLogModel getSvcLogModel(SvcInfo svcInfo) {
         if (RequestContextHolder.getRequestAttributes() != null) {
             if (svcInfo != null) {
-                if (svcInfo.getSvcBinderModel().getSvc().log()) {
-                    SvcLogModel model = new SvcLogModel();
-                    model.setSvcUuid(svcInfo.getUuid());
-                    model.setIp(svcInfo.getIp());
-                    return model;
+                if(svcInfo.getSvcBinderModel() != null) {
+                    if (svcInfo.getSvcBinderModel().getSvc().log()) {
+                        SvcLogModel model = new SvcLogModel();
+                        model.setSvcUuid(svcInfo.getUuid());
+                        model.setIp(svcInfo.getIp());
+                        return model;
+                    }
                 }
             }
         } else {
